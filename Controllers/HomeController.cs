b5fa29a00a8c520106b128b75f011aeabbfca4f6
jcuribev <juanc.uribev@autonoma.edu.co>
2021-11-20 20:19:08 -0500

    vistas de login y registrar. Navbar modificado. Pequeños arreglos en vistas. Modificaciones de modelos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,15 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Register()
+    {
+        return View();
+    }
+    public IActionResult Login()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- Models/Tutor.cs
+++ Models/Tutor.cs
@@ -8,9 +8,13 @@ namespace Tutorias.Models
     public class Tutor
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Email {get;set;}
-        public string Description { get; set; }
+        [MaxLength(64)] public string Name { get; set; }
+        [MaxLength(128)] public string Email { get; set; }
+        [MaxLength(700)] public string Description { get; set; }
+        [MaxLength(10)] public string? PhoneNumber { get; set; }
+        [MaxLength(100)] public string? TwitterLink { get; set; }
+        [MaxLength(100)] public string? FacebookLink { get; set; }
+        [MaxLength(100)] public string? InstagramLink { get; set; }
         public float? AverageScore { get; set; }
         public ICollection<Tutorship>? Tutorships { get; set; }
         public ICollection<TutorCategory>? TutorCategories { get; set; }
